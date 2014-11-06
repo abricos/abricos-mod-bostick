4d0d54c5163d26d6fad8e2b66bc1a40cfb34ef98
Alexander Kuzmin <roosit@abricos.org>
2014-11-06 20:13:20 +0300

    i18n: roles

--- src/js/langs/cp_en.js
+++
@@ -1,6 +0,0 @@
-Brick.util.Language.add('en',{'mod':{'bostick': {
-	'title': 'Stickers',
-	'roles':{
-		'30': 'Write'
-	}
-}}});
--- src/js/langs/cp_ru.js
+++
@@ -1,6 +0,0 @@
-Brick.util.Language.add('ru',{'mod':{'bostick': {
-	'title': 'Стикеры',
-	'roles':{
-		'30': 'Запись'
-	}
-}}});
